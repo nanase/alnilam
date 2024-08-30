17f6fdf1ac6df315917e2604bf4dd7db9c7fe8d4
Nanase <nanase@users.noreply.github.com>
2024-08-31 02:32:15 +0900

    Add new tests for lib/object.ts

--- test/lib/object.test.ts
+++ test/lib/object.test.ts
@@ -1,4 +1,4 @@
-import { deepAssign, RawObject } from '@/lib/object';
+import { deepAssign, RawObject, ternary } from '@/lib/object';
 
 describe('deepAssign', () => {
   test('assign empty object to empty object', () => {
@@ -24,5 +24,24 @@ describe('deepAssign', () => {
 
   test('assign object to nested object', () => {
     expect(deepAssign({ a: 1, b: { c: 2 } }, { b: { c: 3 } })).toStrictEqual({ a: 1, b: { c: 3 } });
+  });
+
+  test('assign null object to object', () => {
+    expect(deepAssign({ a: 1, b: 2 }, { b: null })).toStrictEqual({ a: 1, b: null });
+  });
+});
+
+describe('ternary', () => {
+  test('when condition is truthy', () => {
+    expect(ternary(true, 0, 1, 2)).toBe(0);
+  });
+
+  test('when condition is falsy', () => {
+    expect(ternary(false, 0, 1, 2)).toBe(1);
+  });
+
+  test('when condition is nullish', () => {
+    expect(ternary(undefined, 0, 1, 2)).toBe(2);
+    expect(ternary(null, 0, 1, 2)).toBe(2);
   });
 });
