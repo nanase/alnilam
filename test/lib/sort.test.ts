acfa64965d7807235422e47f7353ac6a2e329370
Nanase <nanase@users.noreply.github.com>
2024-08-31 01:14:14 +0900

    Add tests for lib/sort.ts

---
+++ test/lib/sort.test.ts
@@ -0,0 +1,23 @@
+import { compareWithNull } from '@/lib/sort';
+
+describe('compareWithNull', () => {
+  const array = [3, null, 1, NaN, 2, NaN, 1, null, 0];
+
+  test('sort with null by ascending', () => {
+    expect(array.sort(compareWithNull)).toEqual([0, 1, 1, 2, 3, null, null, NaN, NaN]);
+  });
+
+  test('sort with null by descending', () => {
+    expect(array.sort((a, b) => compareWithNull(a, b, 'descending'))).toEqual([3, 2, 1, 1, 0, null, null, NaN, NaN]);
+  });
+
+  test('compare by ascending', () => {
+    expect(compareWithNull(1, 2, 'ascending')).toEqual(-1);
+    expect(compareWithNull(2, 1, 'ascending')).toEqual(1);
+  });
+
+  test('compare by descending', () => {
+    expect(compareWithNull(1, 2, 'descending')).toEqual(1);
+    expect(compareWithNull(2, 1, 'descending')).toEqual(-1);
+  });
+});
