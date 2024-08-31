3c6d79dbbe068f3f847a8fe8d0dba5d5dfe3a684
Nanase <nanase@users.noreply.github.com>
2024-08-31 23:18:12 +0900

    Update build for 0.2.3

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -5,6 +5,7 @@ export * from './lib/number';
 export * from './lib/object';
 export * from './lib/siPrefix';
 export * from './lib/sort';
+export * from './lib/state';
 export * from './lib/string';
 export * from './lib/theme';
 export * from './lib/vue';
