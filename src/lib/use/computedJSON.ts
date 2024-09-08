feb699d6ebb34d348c52a4c00b23f7535136b75b
Nanase <nanase@users.noreply.github.com>
2024-09-08 15:13:03 +0900

    Fix types of computedJSON

--- src/lib/use/computedJSON.ts
+++ src/lib/use/computedJSON.ts
@@ -6,15 +6,14 @@ export interface ComputedJSONOptions {
   reviver?: Parameters<typeof JSON.parse>[1];
 }
 
-export function computedJSON<T>(source: Ref<T>, options: ComputedJSONOptions = {}): WritableComputedRef<string, T> {
+export function computedJSON<T>(
+  source: Ref<T>,
+  options: ComputedJSONOptions = {},
+): WritableComputedRef<string, string> {
   return computed<string>({
-    get: () => JSON.stringify(source.value, options.replacer, options.space),
+    get: (): string => JSON.stringify(source.value, options.replacer, options.space),
     set: (value: string) => {
-      try {
-        source.value = JSON.parse(value, options.reviver);
-      } catch {
-        console.error('failed to import');
-      }
+      source.value = JSON.parse(value, options.reviver);
     },
   });
 }
