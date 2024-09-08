ce38e4d2331838a43fcd31f9d0d45453c60e9037
Nanase <nanase@users.noreply.github.com>
2024-09-08 15:57:02 +0900

    Fix AnimatedClock export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
+export { default as AnimatedClock } from './AnimatedClock.vue';
 export { default as AppBase } from './AppBase.vue';
 export { default as ThemeToggleButton } from './ThemeToggleButton.vue';
