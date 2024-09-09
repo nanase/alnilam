3cafcbc32ae82f393a175656d2d41f6bbe0321f4
Nanase <nanase@users.noreply.github.com>
2024-09-09 14:15:35 +0900

    Add dayjs modules to rollupOptions.external

--- vite.config.ts
+++ vite.config.ts
@@ -68,7 +68,7 @@ export default defineConfig({
       formats: ['es'],
     },
     rollupOptions: {
-      external: ['vue'],
+      external: ['vue', /^dayjs/],
       output: {
         exports: 'named',
         globals: {
