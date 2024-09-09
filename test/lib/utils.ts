cff15136113d924c66f032649189b018652cddea
Nanase <nanase@users.noreply.github.com>
2024-09-10 04:30:54 +0900

    Add netTwoTick function to test/lib/utils.ts
    この関数はsetTimeoutの特性を逆手にとって、即時ではないが可能な限り短い間隔で待ち時間を発生させる。
    
    関数の参考元:
    https://github.com/vueuse/vueuse/blob/5478f78e6058d169181549b9cc5f2fbf4e2bf8e4/packages/.test/nextTick.ts

--- test/lib/utils.ts
+++ test/lib/utils.ts
@@ -1,3 +1,12 @@
 export async function wait(milliseconds: number) {
   return await new Promise((resolve) => setTimeout(resolve, milliseconds));
 }
+
+// ref: https://github.com/vueuse/vueuse/blob/5478f78e6058d169181549b9cc5f2fbf4e2bf8e4/packages/.test/nextTick.ts
+export function nextTwoTick() {
+  return new Promise<void>((resolve) => {
+    setTimeout(() => {
+      setTimeout(resolve);
+    });
+  });
+}
