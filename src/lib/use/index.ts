83e914a4d98acb3aacc8ff8dd4cf9f986cb979ce
Nanase <nanase@users.noreply.github.com>
2024-09-15 22:46:17 +0900

    Remove useMutationObserver.ts
    全く同じことが実現できる関数がVueUseに存在するので削除

--- src/lib/use/index.ts
+++ src/lib/use/index.ts
@@ -2,4 +2,3 @@ export * from './computedJSON';
 export * from './useElapsedTime';
 export * from './useIntervalFetch';
 export * from './useIntervalFnWithPauser';
-export * from './useMutationObserver';
